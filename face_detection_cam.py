a21eb601bd62226559b1798a30ea543809eb7156
Rahul <r.barman2005@gmail.com>
2021-01-14 22:27:55 +0530

    Initial commit

---
+++ face_detection_cam.py
@@ -0,0 +1,40 @@
+import cv2
+
+imagePath = "cry.jpg"
+cascPath = "face.xml"
+
+webcam = cv2.VideoCapture(0) 
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
